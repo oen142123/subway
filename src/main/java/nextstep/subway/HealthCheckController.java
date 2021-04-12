87105627b25bfc138077de33cb0e63ac1f3b8ae5
wani <oen142@naver.com>
2021-04-12 16:31:07 +0900

    testtest

--- src/main/java/nextstep/subway/HealthCheckController.java
+++ src/main/java/nextstep/subway/HealthCheckController.java
@@ -1,13 +1,20 @@
 package nextstep.subway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
 
+    private static final Logger logger = LoggerFactory.getLogger(HealthCheckController.class);
+
     @GetMapping("/api/v1")
     public String check() {
+        logger.info("api UP");
+        logger.error("api UP");
+        logger.debug("api UP");
         return "UP";
     }
 }
